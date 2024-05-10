1d20dfa443f5d1324946b276032e95497cf20f56
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-05-10 14:07:41 +0300

    loop for items

--- lib/screen/categories.dart
+++ lib/screen/categories.dart
@@ -1,53 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shcool/data/dummy_data.dart';
+import 'package:shcool/widgets/category_grid_item.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Вибери свій клас!'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(50),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+        appBar: AppBar(
+          title: const Text('Вибери свій клас!'),
         ),
-        children: [
-          _buildCategoryCircle('1', Colors.purple),
-          _buildCategoryCircle('2', Colors.red),
-          _buildCategoryCircle('3', Colors.orange),
-          _buildCategoryCircle('4', Colors.amber),
-          _buildCategoryCircle('5', Colors.blue),
-          _buildCategoryCircle('6', Colors.green),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCategoryCircle(String text, Color color) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+              ),
+              children: [
+                for (final category in availableCategories)
+                  CategoryGridItem(category: category)
+              ]),
+        ));
   }
 }
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,13 +1,34 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shcool/model/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({super.key, required this.category});
-
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.9),
+            category.color.withOpacity(1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Text(
+          category.title,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
   }
 }
