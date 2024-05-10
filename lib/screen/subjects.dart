ebfcae1408b7bbfca7eba0eb46b31bb9b77af4ba
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-05-10 19:51:07 +0300

    buttons for items

--- lib/screen/subjects.dart
+++ lib/screen/subjects.dart
@@ -4,25 +4,48 @@ import 'package:shcool/widgets/subject_grid_item.dart';
 
 class SubjectScreen extends StatelessWidget {
   const SubjectScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Вибери предмет!'),
+      appBar: AppBar(
+        title: const Text('Навчайся або грайся :)'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (final category in availableSubjects)
+                Column(
+                  children: [
+                    SubjectsGridItem(category: category),
+                    // Простір між карткою і кнопками
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Дії при натисканні на першу кнопку
+                          },
+                          child: const Text('Вчити'),
+                        ),
+                        const SizedBox(width: 60),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Дії при натисканні на другу кнопку
+                          },
+                          child: const Text('Грати'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+            ],
+          ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(30),
-          child: GridView(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-              ),
-              children: [
-                for (final category in availableSubjects)
-                  SubjectsGridItem(category: category)
-              ]),
-        ));
+      ),
+    );
   }
 }
--- lib/widgets/subject_grid_item.dart
+++ lib/widgets/subject_grid_item.dart
@@ -7,30 +7,27 @@ class SubjectsGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      splashColor: Theme.of(context).primaryColor,
-      borderRadius: BorderRadius.circular(50),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.9),
-              category.color.withOpacity(1),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+    return Container(
+      width: 300, // Фіксована ширина картки
+      height: 80,
+      padding: const EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.9),
+            category.color.withOpacity(1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: Center(
-          child: Text(
-            category.title,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-                fontWeight: FontWeight.bold),
-          ),
+      ),
+      child: Center(
+        child: Text(
+          category.title,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+              fontWeight: FontWeight.bold),
         ),
       ),
     );
