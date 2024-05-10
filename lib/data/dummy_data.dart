1cc7a495d3de62272b706c26f9fc8b18290c50cb
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-05-10 13:00:47 +0300

    model

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shcool/model/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: '1',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: '2',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: '3',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: '4',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: '5',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: '6',
+    color: Colors.green,
+  ),
+];
