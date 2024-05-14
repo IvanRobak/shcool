48bba07581bb5bf7009485b72cdfb13412c1cafc
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-05-14 15:27:18 +0300

    create  StudyItem

--- lib/widgets/study_tem.dart
+++ lib/widgets/study_tem.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class StudyItem extends StatelessWidget {
+class StudyItem extends StatefulWidget {
   const StudyItem({
     super.key,
     required this.imagePath,
@@ -11,21 +11,79 @@ class StudyItem extends StatelessWidget {
   final String title;
 
   @override
+  State<StudyItem> createState() => _StudyItemState();
+}
+
+class _StudyItemState extends State<StudyItem> {
+  bool isFavorite = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(child: Image.asset(imagePath)),
-        Text(
-          title,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 20,
-        )
-      ],
+    return Container(
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: const Color.fromARGB(255, 66, 64, 64),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(8)),
+                child: Image.asset(
+                  widget.imagePath,
+                  fit: BoxFit.cover,
+                  height: 277,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Text(
+                      widget.title,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: IconButton(
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : null,
+              ),
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
