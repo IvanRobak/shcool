703e256386c37d2906d56f389f917fb77ebb7b90
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-05-27 10:54:40 +0300

    cadr_detail

--- lib/screen/card_detail.dart
+++ lib/screen/card_detail.dart
@@ -12,14 +12,27 @@ class CardDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(card.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(50),
-        child: Center(
-          child: Text(card.description,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.black.withOpacity(0.5),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Center(
+            child: Text(card.description,
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground)),
+          ),
         ),
       ),
     );
